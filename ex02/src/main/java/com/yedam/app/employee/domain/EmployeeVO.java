b7f7451a9b5ce007a237c5cfb7d585d78d88363d
Phoebe Baek <84132641+cheesepeperoni@users.noreply.github.com>
2022-04-03 19:15:36 +0900

    Delete ex02 directory

--- ex02/src/main/java/com/yedam/app/employee/domain/EmployeeVO.java
+++
@@ -1,5 +0,0 @@
-package com.yedam.app.employee.domain;
-
-public class EmployeeVO {
-
-}
--- ex02/src/main/java/com/yedam/app/sample/service/SampleService.java
+++
@@ -1,6 +0,0 @@
-package com.yedam.app.sample.service;
-
-public interface SampleService {
-	
-	public void addData(String value);
-}
